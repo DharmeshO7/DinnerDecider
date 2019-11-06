e7df5651660d25c197226ad1daa91e54ad65d417
Dharmesh <dharmeshpadhiyar77@gmail.com>
2019-11-06 18:11:50 +0530

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+q
+Aapp/src/main/java/dharmesh/padhiyar/dinnerdecider/MainActivity.kt,5\2\522dc005395bc55e9f692b8ecc7fe1a5e6600762
---
+++ app/src/main/java/dharmesh/padhiyar/dinnerdecider/MainActivity.kt
@@ -0,0 +1,44 @@
+package dharmesh.padhiyar.dinnerdecider
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Burger", "Pizza", "Sandwich", "Pasta", "Biryani", "Maggi")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val random = Random()
+
+        decideButton.setOnClickListener {
+
+            val randomFood = random.nextInt(foodList.count())
+            val randomFoodName = foodList[randomFood]
+
+            if (randomFoodName.equals(selectedFood.text)) {
+                decideButton.callOnClick()
+            } else {
+                selectedFood.text = foodList[randomFood]
+            }
+        }
+
+        addFoodButton.setOnClickListener {
+
+            val foodName = addFoodTxt.text.toString()
+
+            if (!foodName.isEmpty()) {
+                foodList.add(foodName)
+                addFoodTxt.text.clear()
+                Toast.makeText(this, "Food added! Go and Decide.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Enter something fool!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
